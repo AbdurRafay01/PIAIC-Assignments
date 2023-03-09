69b46b746c56f89f3c1017cb040187dfb858e614
Abdur Rafay <cornal008@gmail.com>
2023-03-10 02:00:41 +0500

    todo app

--- todo-cdn-react-babel/app.js
+++ todo-cdn-react-babel/app.js
@@ -1,22 +1,32 @@
-'use strict'
+"use strict";
 
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 
-console.log('hello');
-
-
+console.log("hello");
 
 const App = () => {
   const { useState } = React;
   const [tasks, setTasks] = useState([]);
 
+
   const handleSubmit = (e) => {
+    console.log(e.target);
     let taskName = e.target.task.value;
-    setTasks([...tasks, taskName])
+    setTasks([...tasks, taskName]);
     // setTasks()
     e.preventDefault();
-  };
+  }
+
+  const handleDelete = (e) => {
+    console.log(e.target.value);
+    let index = e.target.value;
+    let newTasks = _.clone(tasks);
+    console.log('newTasks', typeof(newTasks));
+    console.log('tasks', typeof(tasks));
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  }
 
   return (
     <>
@@ -24,8 +34,11 @@ const App = () => {
       <ul>
         {tasks.map((obj, i) => {
           return (
-            <li key={i}>{obj}</li>
-          )
+            <li key={i}>
+              {obj}
+              <button name="deleteButton" value={i} onClick={handleDelete}>Delete</button>
+            </li>
+          );
         })}
       </ul>
       <form onSubmit={handleSubmit}>
@@ -33,10 +46,10 @@ const App = () => {
         <button type="submit">Add</button>
       </form>
     </>
-  )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
